e2ffcec39ad50a14f52e535d0f35d77db34657dc
Boris Brodski <boris_brodski@users.sourceforge.net>
2010-12-12 14:22:55 +0100

    Redmine plugin created.
    Empty redmine plugin 'fbee' created.
    README.rdoc contains very simple project description.

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_fbee do
+  name 'Redmine feature branch enabled environment (FBEE) plugin'
+  author 'Boris Brodski'
+  description 'This is a feature branch enabled environment (FBEE) plugin for redmine.'
+  version '0.0.1'
+#  url 'http://example.com/path/to/plugin'
+#  author_url 'http://example.com/about'
+end
