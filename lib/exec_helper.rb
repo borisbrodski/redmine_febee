ad1082c5d9bdd9c7d4f87ed9b57721d6f8e9f278
Boris Brodski <boris_brodski@users.sourceforge.net>
2010-12-26 01:35:05 +0100

    Support array of arguments. This should make quoting simpler.

--- lib/exec_helper.rb
+++ lib/exec_helper.rb
@@ -5,13 +5,13 @@ module ExecHelper
   include Redmine::I18n
   
   class ExecError < StandardError
-    attr :cmd
+    attr :cmds
     attr :description
     attr :exit_code
     attr :std_out
     attr :std_err
-    def initialize(cmd, description, exit_code = nil, std_out = nil, std_err = nil)
-      @cmd = cmd
+    def initialize(cmds, description, exit_code = nil, std_out = nil, std_err = nil)
+      @cmds = cmds
       @description = description
       @exit_code = exit_code
       @std_out = std_out
@@ -20,32 +20,32 @@ puts "ExecError: #{message}" # TODO use logger
     end
     
     def message
-      message = "#{to_s}\nTask: #{description}\nCmd: #{cmd}"
+      message = "#{to_s}\nTask: #{description}\nCmd: \"#{cmds.join '" "'}\""
       message <<= "\nStandard output: '#{std_out}'" unless std_out.blank?
       message <<= "\nStandard error: '#{std_err}'" unless std_err.blank?
     end
   end
   
-  def run_cmd(cmd, description)
+  def run_cmd(description, *cmds)
     output = nil
     error = nil
-puts "#{description} => Running: '#{cmd}'" # TODO use logger
+puts "#{description} => Running: '#{cmds.join " "}'" # TODO use logger
     begin
-      status = Open4::popen4(cmd) do |pid, stdin, stdout, stderr|
+      status = Open4::popen4(*cmds) do |pid, stdin, stdout, stderr|
         output = stdout.read
         error = stderr.read
       end
     rescue Exception => e
       # TODO add caused by
-      raise ExecError.new(cmd, description), l(:exec_error_cant_start_program) + ": #{e}"
+      raise ExecError.new(cmds, description), l(:exec_error_cant_start_program) + ": #{e}"
     end
-    raise ExecError.new(cmd, description), l(:exec_error_cant_start_program)  if status == nil
+    raise ExecError.new(cmds, description), l(:exec_error_cant_start_program)  if status == nil
     exit_code = status.exitstatus
     if exit_code == 0
 puts "Success\nStdOut: '#{output}'\nStdErr: '#{error}'"# TODO use logger
       output
     else
-      raise ExecError.new(cmd, description, exit_code, output, error), l(:exec_error_cant_start_program)
+      raise ExecError.new(cmds, description, exit_code, output, error), l(:exec_error_cant_start_program)
     end
   end
 end
