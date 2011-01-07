f04af070e9b1d2cf0c1bd3836da1314ad773df4a
Boris Brodski <boris_brodski@users.sourceforge.net>
2011-01-07 11:28:14 +0100

    Add update interval to the febee_project_configurations

--- app/models/febee_workspace.rb
+++ app/models/febee_workspace.rb
@@ -2,4 +2,16 @@ class FebeeWorkspace < ActiveRecord::Base
   unloadable
   
   belongs_to :febee_project_configuration
+  
+  # Returns updated GitRepository object
+  def git_repository
+    @git_repository ||= GitRepository.new(febee_project_configuration)
+    PerRequestCache.fetch(:update_workspace) do
+      if update_interval && last_git_fetch
+        return if last_git_fetch > update_interval.seconds.ago
+      end
+      @git_repository.fetch_from_server
+    end
+    @git_repository
+  end
 end
---
+++ db/migrate/06_add_update_interval_to_febee_project_configuration.rb
@@ -0,0 +1,13 @@
+class AddUpdateIntervalToFebeeProjectConfiguration < ActiveRecord::Migration
+  def self.up
+    change_table :febee_project_configurations do |t|
+      t.integer :update_interval
+    end
+  end
+  
+  def self.down
+    change_table :febee_project_configurations do |t|
+      r.remove :update_interval
+    end
+  end
+end
